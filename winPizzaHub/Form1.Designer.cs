287f8445787ef521333bfe9e3f91e92834132039
Podguzkov Artem <podguzkov@mail.ru>
2025-12-25 19:59:01 +0300

    comit3

--- winPizzaHub/Form1.Designer.cs
+++ winPizzaHub/Form1.Designer.cs
@@ -180,7 +180,7 @@ namespace winPizzaHub
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoScroll = true;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(818, 561);
+            this.ClientSize = new System.Drawing.Size(835, 561);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.label2);
